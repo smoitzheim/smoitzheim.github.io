30c4f3fbe06ace667af6320304183b61f290e30d
Sebastian Moitzheim <smoitzheim@Mac.fritz.box>
2025-01-23 02:56:03 +0100

    micro test

--- eleventy.config.js
+++ eleventy.config.js
@@ -107,7 +107,12 @@ eleventyConfig.addPlugin(pluginSyntaxHighlight, {
 			}
 		}
 	});
+	const excerpt = require('eleventy-plugin-excerpt');
 
+	module.exports = function(eleventyConfig) {
+  	eleventyConfig.addPlugin(excerpt);
+	};
+	
 	eleventyConfig.addPlugin(pluginFilters);
 	eleventyConfig.addPlugin(IdAttributePlugin, {
 	});
